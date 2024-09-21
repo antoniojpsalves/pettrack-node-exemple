245ca561e979b4dfce07c963442eb5bec6397986
Antonio Alves <antonio.jpsalves@gmail.com>
2024-09-20 21:34:51 -0300

    :white_check_mark: verificando se o email ja foi cadastrado anteriormente

--- src/use-cases/users/register.spec.ts
+++ src/use-cases/users/register.spec.ts
@@ -2,6 +2,7 @@ import { expect, describe, it } from 'vitest'
 import { RegisterUsers } from './registerUser'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '../../repositories/users/in-memony-users-repository'
+import { UserEmailAlreadyExistsError } from './errors/user-email-already-exists-error'
 
 describe('Register use case', () => {
   it('should hash user password upon registration', async () => {
@@ -10,20 +11,44 @@ describe('Register use case', () => {
 
     const registerUseCase = new RegisterUsers(userRepositoryMock)
 
-    const { user } = await registerUseCase.execute({
+    const fakeData = {
       name: 'johnathan',
       email: 'johnathan@google.com',
       password: '12345678',
       cep: '019281210',
       cpf_cnpj: '129291910029',
       is_active: true,
-    })
+    }
+
+    const { user } = await registerUseCase.execute(fakeData)
 
     const isPasswordCorretlyHashed = await compare(
-      '12345678',
+      fakeData.password,
       user.password_hash,
     )
 
     expect(isPasswordCorretlyHashed).toBe(true)
+  })
+
+  it('should not to be able to register with same email type', async () => {
+    const userRepositoryMock = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUsers(userRepositoryMock)
+
+    const fakeEmail = 'johnathan@google.com'
+
+    const fakeData = {
+      name: 'johnathan',
+      email: fakeEmail,
+      password: '12345678',
+      cep: '019281210',
+      cpf_cnpj: '129291910029',
+      is_active: true,
+    }
+
+    await registerUseCase.execute(fakeData)
+
+    expect(() => registerUseCase.execute(fakeData)).rejects.toBeInstanceOf(
+      UserEmailAlreadyExistsError,
+    )
   })
 })
