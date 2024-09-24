303d371d30abb64717ec3851b896353c7ad7cc73
Antonio Alves <antonio.jpsalves@gmail.com>
2024-09-24 01:08:58 -0300

    :sparkles: feat: authenticate usecase

---
+++ src/use-cases/authenticate/authenticate.ts
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+import { compare } from 'bcryptjs'
+import { UsersRepository } from '../../repositories/users/users-repository'
+import { InvalidCredentialsError } from '../users/errors/invalid-credentials-error'
+import { AuthenticateUseCaseRequest } from './dtos/authenticateUserDto'
+import { AuthenticateUserCaseResponse } from './dtos/authenticateUserResponse'
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) { }
+
+  // auth a user
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUserCaseResponse> {
+
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordsMatch = compare(password, user.password_hash)
+
+    if (!doesPasswordsMatch) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
